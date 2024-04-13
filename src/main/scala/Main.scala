b184685ef5e79f08f0c9b9ee33abbd65479398b2
Steve Hussung <steven.hussung@gmail.com>
2024-04-13 01:14:15 -0400

    We can list the stops on each schedule now. We can list the times too! But it's too much information without formatting.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,6 @@ object JsoupScraper:
 
   @main def hello(): Unit =
     println("Hello world!")
-    println(msg)
 
     //Beginning to test jsoup. Following https://www.scrapingbee.com/blog/web-scraping-scala/
     /*
@@ -17,18 +16,24 @@ object JsoupScraper:
     println("\nDid you know:")
     println(wiki.select("#mp-dyk b a"))
     // */
-    //
+
     val bus_route = Jsoup.connect("https://www.indygo.net/route/3-michigan-street/").get()
-    println("\nBus route 3")
-    println(bus_route.select("#accordion-weekday-0"))
+    // println("\nBus route 3")
+    // println(bus_route.select("#accordion-weekday-0"))
     
-    println("\nOne stop on the route: Michigan and Rural")
-    println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1)"))
-    println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2)"))
+    // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1)"))
+    // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2)"))
     
-    val bus_stop = bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li").asScala
-    for time <- bus_stop do
-      println("Hey! I'm an element")
-      println(time)
-      println(time.text)
-  def msg = "I was compiled by Scala 3. :)"
+    for schedule <- bus_route.select("#stop_lists > *").asScala 
+    do
+      println("\nSCHEDULE ELEMENT HERE")
+      println(schedule.attr("id"))
+      // println(schedule.text)
+      for card <- schedule.select("[class=card]").asScala
+      do
+        println(card.select("div h5 button span").text)
+        // This lists through the stops, which is too much information at this point.
+        // for stop <- schedule.select("li").asScala
+        // do
+          // println(stop.text)
+
