4848755b0817ac7e27a7eb6a6c0dadeba0c351c0
Steve Hussung <steven.hussung@gmail.com>
2024-04-14 18:30:41 -0400

    Removing comments and old code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,24 +4,7 @@ import scala.jdk.CollectionConverters._
 @main def hello(): Unit =
   println("Hello world!")
 
-  //Beginning to test jsoup. Following https://www.scrapingbee.com/blog/web-scraping-scala/
-  /*
-  val wiki = Jsoup.connect("http://en.wikipedia.org/").get()
-  print("\nLet's parse ... ")
-  println(wiki.title())
-  println("\nIn the news:")
-  println(wiki.select("#mp-itn b a"))
-  println("\nDid you know:")
-  println(wiki.select("#mp-dyk b a"))
-  // */
-
   val bus_route_page = Jsoup.connect("https://www.indygo.net/route/3-michigan-street/").get()
-  // println("\nBus route 3")
-  // println(bus_route.select("#accordion-weekday-0"))
-  
-  // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1)"))
-  // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2)"))
-  
   val bus_route = bus_route_reader(bus_route_page)
   
   println("Attempt to print out the bus route in an organized way:")
