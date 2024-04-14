06a67f05e471c91949a6b68d2d912d8d7be4adee
Steve Hussung <steven.hussung@gmail.com>
2024-04-14 18:30:23 -0400

    Extracting bus_route_reader code into dedicated function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -22,22 +22,7 @@ import scala.jdk.CollectionConverters._
   // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1)"))
   // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2)"))
   
-  val bus_route = 
-  for schedule <- bus_route_page.select("#stop_lists > *").asScala 
-  yield
-    var schedule_id_info = schedule.attr("id").split("-")
-    var day_of_week = schedule_id_info(1).capitalize
-    var direction = if schedule_id_info(2) == "0" then "Westbound" else "Eastbound"
-
-    var stop_list =
-      (
-      for card <- schedule.select("[class=card]").asScala
-      yield
-        var stop_name = card.select("div h5 button span").text
-        var stop_times = card.select("li").asScala.map(_.text)
-        (stop_name -> stop_times)
-      )
-    ((day_of_week, direction) -> stop_list)
+  val bus_route = bus_route_reader(bus_route_page)
   
   println("Attempt to print out the bus route in an organized way:")
   for ((weekdarity, direction), stops) <- bus_route
@@ -47,5 +32,22 @@ import scala.jdk.CollectionConverters._
     for (stop_name, stop_times) <- stops
     do
       println(stop_name)
-      // println(s"There are ${stop_times.length} times at this stop")
       println(stop_times)
+    
+def bus_route_reader(bus_route_page : org.jsoup.nodes.Document) =
+  for schedule <- bus_route_page.select("#stop_lists > *").asScala 
+  yield
+    var schedule_id_info = schedule.attr("id").split("-")
+    var day_of_week = schedule_id_info(1).capitalize
+    var direction = if schedule_id_info(2) == "0" then "Westbound" else "Eastbound"
+
+    ((day_of_week, direction) ->
+      (
+      for card <- schedule.select("[class=card]").asScala
+      yield
+        var stop_name = card.select("div h5 button span").text
+        var stop_times = card.select("li").asScala.map(_.text)
+        (stop_name -> stop_times)
+      )
+    )
+    
