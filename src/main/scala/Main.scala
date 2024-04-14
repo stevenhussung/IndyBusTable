404b98059867ce3bec6b7c1df406a3803c14601f
Steve Hussung <steven.hussung@gmail.com>
2024-04-14 18:26:04 -0400

    Successfully read data into data structure and parsed data structure for structured printout. Also fixed error in parsing stop times. We were going too broad.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,52 +1,51 @@
 import org.jsoup._
 import scala.jdk.CollectionConverters._
 
-object JsoupScraper: 
+@main def hello(): Unit =
+  println("Hello world!")
 
-  @main def hello(): Unit =
-    println("Hello world!")
+  //Beginning to test jsoup. Following https://www.scrapingbee.com/blog/web-scraping-scala/
+  /*
+  val wiki = Jsoup.connect("http://en.wikipedia.org/").get()
+  print("\nLet's parse ... ")
+  println(wiki.title())
+  println("\nIn the news:")
+  println(wiki.select("#mp-itn b a"))
+  println("\nDid you know:")
+  println(wiki.select("#mp-dyk b a"))
+  // */
 
-    //Beginning to test jsoup. Following https://www.scrapingbee.com/blog/web-scraping-scala/
-    /*
-    val wiki = Jsoup.connect("http://en.wikipedia.org/").get()
-    print("\nLet's parse ... ")
-    println(wiki.title())
-    println("\nIn the news:")
-    println(wiki.select("#mp-itn b a"))
-    println("\nDid you know:")
-    println(wiki.select("#mp-dyk b a"))
-    // */
+  val bus_route_page = Jsoup.connect("https://www.indygo.net/route/3-michigan-street/").get()
+  // println("\nBus route 3")
+  // println(bus_route.select("#accordion-weekday-0"))
+  
+  // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1)"))
+  // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2)"))
+  
+  val bus_route = 
+  for schedule <- bus_route_page.select("#stop_lists > *").asScala 
+  yield
+    var schedule_id_info = schedule.attr("id").split("-")
+    var day_of_week = schedule_id_info(1).capitalize
+    var direction = if schedule_id_info(2) == "0" then "Westbound" else "Eastbound"
 
-    val bus_route = Jsoup.connect("https://www.indygo.net/route/3-michigan-street/").get()
-    // println("\nBus route 3")
-    // println(bus_route.select("#accordion-weekday-0"))
-    
-    // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(1)"))
-    // println(bus_route.select("#accordion-weekday-0-collapse-1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(2)"))
-    
-    for schedule <- bus_route.select("#stop_lists > *").asScala 
+    var stop_list =
+      (
+      for card <- schedule.select("[class=card]").asScala
+      yield
+        var stop_name = card.select("div h5 button span").text
+        var stop_times = card.select("li").asScala.map(_.text)
+        (stop_name -> stop_times)
+      )
+    ((day_of_week, direction) -> stop_list)
+  
+  println("Attempt to print out the bus route in an organized way:")
+  for ((weekdarity, direction), stops) <- bus_route
+  do
+    println(weekdarity)
+    println(direction)
+    for (stop_name, stop_times) <- stops
     do
-      var schedule_id_info = schedule.attr("id").split("-")
-
-      var day_of_week = schedule_id_info(1).capitalize
-      println(s"\nDay of week is: ${day_of_week}")
-
-      var direction = if schedule_id_info(2) == "0" then "Westbound" else "Eastbound"
-      println(s"Direction is: ${direction}")
-
-      var stop_list =
-        (
-        for card <- schedule.select("[class=card]").asScala
-        yield
-          var stop_name = card.select("div h5 button span").text
-          // println(s"Stop name: ${stop_name}")
-          var stop_times = List("times")
-          /*
-          for stop <- schedule.select("li").asScala
-          yield
-            stop.text
-          */
-          (stop_name -> stop_times)
-        )
-      // println(stop_list)
-      
+      println(stop_name)
+      // println(s"There are ${stop_times.length} times at this stop")
+      println(stop_times)
