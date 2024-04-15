f7b5bda7ee86ca384d35831b09e20338e41dc4eb
Steve Hussung <steven.hussung@gmail.com>
2024-04-14 22:27:44 -0400

    Html generation version 1 up and running

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,21 +2,50 @@ import org.jsoup._
 import scala.jdk.CollectionConverters._
 import scalatags.Text.all._
 
+import java.io._
+
 @main def hello(): Unit =
   println("Hello world!")
 
   val bus_route_page = Jsoup.connect("https://www.indygo.net/route/3-michigan-street/").get()
-  val bus_route = bus_route_reader(bus_route_page)
+  val bus_route = bus_route_reader(bus_route_page).sortBy(_(0)).reverse
   
-  println("Attempt to print out the bus route in an organized way:")
-  for ((weekdarity, direction), stops) <- bus_route
-  do
-    println(weekdarity)
-    println(direction)
-    for (stop_name, stop_times) <- stops
-    do
-      println(stop_name)
-      println(stop_times)
+  // for ((weekdarity, direction), stops) <- bus_route
+  // do
+    // println(weekdarity)
+    // println(direction)
+    // for (stop_name, stop_times) <- stops
+    // do
+      // println(stop_name)
+      // println(stop_times)
+  
+  println("\n\nAnd now, some html:")
+  val html_content = 
+    html(
+      head(
+        script("some script")
+      ),
+      body
+      (
+        h1("Route 3: Michigan St."),
+        (
+        for ((weekdarity, direction), stops) <- bus_route
+        yield
+          div(
+            h2(weekdarity ++ ": " ++ direction),
+            for (stop_name, stop_times) <- stops
+            yield
+            p(stop_name + "\n" + stop_times.mkString(" -- "))
+          )
+        ).toList
+      )
+    )
+
+  val output_file = File("output.html")
+  val file_writer = BufferedWriter(FileWriter(output_file))
+  file_writer.write(html_content.toString)
+  file_writer.close()
+
     
 def bus_route_reader(bus_route_page : org.jsoup.nodes.Document) =
   for schedule <- bus_route_page.select("#stop_lists > *").asScala 
@@ -32,6 +61,6 @@ def bus_route_reader(bus_route_page : org.jsoup.nodes.Document) =
         var stop_name = card.select("div h5 button span").text
         var stop_times = card.select("li").asScala.map(_.text)
         (stop_name -> stop_times)
-      )
+      ).toList
     )
     
